b55d34bca2fbdc26bcaf4cc222de4b3401d00436
fieryWalrus1002 <woodmagnus@gmail.com>
2022-09-12 16:36:32 -0700

    unk1

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,6 @@ bool measureState = false; // state for measurements
 Adafruit_SSD1306 display(128, 32, &Wire, OLED_RESET);
 MAX1132 adc;
 
-// define pins
-const int CS_PIN = 10;
-const int ADC_PIN = 15;
-const int SAT_PULSE_PIN = 0;
-const int STO_FLASH_PIN = 0;
-const int meas_led_array[] = {0, 2, 3, 4, 5, 6, 7, 8, 9}; // 0 will not flash anything
 
 struct Point{
     long time_us;
--- src/main.h
+++ src/main.h
@@ -1,5 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
+// define pins
+const int CS_PIN = 10;
+const int ADC_PIN = 15;
+const int SAT_PULSE_PIN = 0;
+const int STO_FLASH_PIN = 0;
+const int meas_led_array[] = {0, 2, 3, 4, 5, 6, 7, 8, 9}; // 0 will not flash anything
+
 void send_data_point(int);
 void write_act_intensity(int);
 void set_act_intensity(int);
