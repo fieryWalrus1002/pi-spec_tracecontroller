58a0a9b5526e7fb0725f6a1033b14e8f0071240d
fieryWalrus1002 <woodmagnus@gmail.com>
2023-02-27 08:25:55 -0800

    notes for next thing

--- src/pins.h
+++ src/pins.h
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 
+// new pins
+const int 
+
+
 class Pins {
     public:
         // define pins
-        const int SAT_PULSE_GATE = 38;
-        const int DETECTOR_GATE_PIN = 32;
         // 30  - ADC_CS_PINT
         // 29 - ADC_RST_PIN
         // 28 - ADC_SSTRB_PIN for max1132 input signal of adc ready state
-        const int MCP_CS_PIN = 23;
-        const int ACT_GATE_PIN = 22;
+        const int MCP_MEAS_CS_PIN = 23;
+        const int MCP_ACT_CS_PIN = 10;
         const int POWER_GATE_PIN = 21;
         const int STO_FLASH_PIN = 20;
         // 19 - SCL - SSD1306 IC2
@@ -26,7 +28,7 @@ class Pins {
         7,  // 940nm
         8, // 820nm
         9}; // 0 will not flash anything
-        const int pin_setup_array[6] {SAT_PULSE_GATE, DETECTOR_GATE_PIN, MCP_CS_PIN, ACT_GATE_PIN, POWER_GATE_PIN, STO_FLASH_PIN};
+        const int pin_setup_array[6] {SAT_PULSE_GATE, DETECTOR_GATE_PIN, MCP_MEAS_CS_PIN, MCP_ACT_CS_PIN, ACT_GATE_PIN, POWER_GATE_PIN, STO_FLASH_PIN};
 
         void init();
         void meas_led_cleanup();
